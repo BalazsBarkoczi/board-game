6985ff79e43c6cdf410316e0c87e370d7b8e4928
Barkóczi Balázs <f5hx5h@inf.elte.hu>
2025-01-27 19:38:02 +0100

    Check neighbouring cells

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   const ROW_COUNT = 5;
   const COL_COUNT = 5;
+  const MIN_LINE_LENGTH = 3;
 
   const [board, setBoard] = useState(Array(ROW_COUNT).fill(Array(COL_COUNT).fill('')));
 
@@ -15,8 +16,6 @@ function App() {
   const [currentCharIndex, setCurrentCharIndex ] = useState(0);
 
   const handleCellClick = (row,col) =>{
-    console.log(board)
-
     if(board[row][col] !== '' || gameOver) return;
 
     const updatedBoard = board.map((boardRows,i)=>(
@@ -28,6 +27,8 @@ function App() {
     setBoard(updatedBoard);
     setCurrentCharIndex((prev) => (prev + 1) % 3 );
 
+    countLines(updatedBoard,row,col);
+
     if(!updatedBoard.flat().some((cell) => cell === '') ){
       setGameOver(true);
     }
@@ -37,6 +38,36 @@ function App() {
     setBoard(Array(ROW_COUNT).fill(Array(COL_COUNT).fill('')));
     setCurrentCharIndex(0);
     setGameOver(false);
+  }
+
+  const countLines = (board, row, col) =>{
+    console.log("CURR:",row,col);
+
+    const activeChar = board[row][col];
+    const lines = 0;
+
+    const directions = [
+      [1,0], //down
+      [0,1], //right
+      [1,1], //bottom right
+      [1,-1] //bottom left
+    ];
+
+    let count = 0;
+
+    directions.forEach( ([dirX,dirY]) => {
+
+      let length = 0;
+      
+      let x = dirX + row;
+      let y = dirY + col;
+
+      console.log(count,board[x][y])
+
+      count++;
+    });
+
+
   }
 
 
